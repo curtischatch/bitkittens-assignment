e78624af5979eebc29e9c1ad5cee17876ab05758
Curtis C <curtis.chat.hk@gmail.com>
2018-09-12 12:40:00 -0400

    set up summon kitten event handler and test with console.log

--- cats.js
+++ cats.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
+  var summonbtn = document.querySelector('button.summon-cats')
+
+  summonbtn.addEventListener('click', function(){
+    console.log("success")
+  })
 
 
+
+  
 
 
 
