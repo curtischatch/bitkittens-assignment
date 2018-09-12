27f765782df5c88643f204e401aec8ad6d578095
Curtis C <curtis.chat.hk@gmail.com>
2018-09-12 14:37:50 -0400

    insert img into one of the empty .cat-box divs

--- cats.js
+++ cats.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // var catBox = querySelector('cat-box')
   var summonbtn =  document.querySelector('button.summon-cats')
+  var box = document.querySelector('#cat1')
 
   summonbtn.addEventListener('click', function(){
     $.ajax({
@@ -10,7 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
       dataType: 'JSON'
     }).done(function(responseData) {
       console.log(responseData)
+      responseData.cats.forEach(function(cat) {
+        var listImg = document.createElement('img')
+        listImg.src = cat.photo
+        box.append(listImg)
+      })
     });
+
+
 
   });
 
